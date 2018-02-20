47ec8ef1979dbf7ebbbcc650e30b2cd03c1b11e5
blairLikesPizza <blairwo@gmail.com>
2018-02-20 12:09:19 -0700

    styling routing and store

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Landing from './components/Landing/Landing.js';
+import Login from './components/Login/Login.js';
+import Lobby from './components/Lobby/Lobby.js';
+import Profile from './components/Profile/Profile.js';
+import LeaderBoard from './components/LeaderBoard/LeaderBoard.js';
+import EditProfile from './components/EditProfile/EditProfile.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+export default (
+
+    <Router>
+        <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/lobby" component={Lobby} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/leaderboard" component={LeaderBoard} />
+            <Route path="/editprofile" component={EditProfile} />
+        </div>
+    </Router>
+)
+
