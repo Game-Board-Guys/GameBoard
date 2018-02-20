1f8e6e5cb8d317a6763ec83d2ee55377245696d7
FoxChrisRealTheGit <cfox1290@gmail.com>
2018-02-20 16:01:28 -0700

    game test additions

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Landing from './components/Landing/Landing.js';
 import Login from './components/Login/Login.js';
-import Lobby from './components/Lobby/Lobby.js';
+//import Lobby from './components/Lobby/Lobby.js';
 import Profile from './components/Profile/Profile.js';
 import LeaderBoard from './components/LeaderBoard/LeaderBoard.js';
 import EditProfile from './components/EditProfile/EditProfile.js';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import BrickBreak from './components/Lobby/GameLibrary/BrickBreakerPureJS';
 
 export default (
 
@@ -13,7 +14,7 @@ export default (
         <div>
             <Route exact path="/" component={Landing} />
             <Route path="/login" component={Login} />
-            <Route path="/lobby" component={Lobby} />
+            <Route path="/lobby" component={BrickBreak} />
             <Route path="/profile" component={Profile} />
             <Route path="/leaderboard" component={LeaderBoard} />
             <Route path="/editprofile" component={EditProfile} />
