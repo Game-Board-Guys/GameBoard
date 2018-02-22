2cd3311575e51e4ad18d56f8ebcec986a3ba1322
kevbir007 <kevbir007@gmail.com>
2018-02-22 16:52:16 -0700

    editpage

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -3,6 +3,7 @@ import './EditProfile.css';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import axios from 'axios';
 
 
 class EditProfile extends Component {
@@ -10,16 +11,64 @@ class EditProfile extends Component {
         super(props);
 
         this.state = {
-
+            username: '',
+            aboutMe: '',
+            id: 0
         }
+        this.handleImgClick = this.handleImgClick.bind(this);
     }
+
+    setUsername(value){
+        this.setState({
+            username: value
+        })
+    }
+
+    setAboutMe(value){
+        this.setState({
+            aboutMe: value
+        })
+    }
+
+    handleImgClick() {
+        console.log(this.state.id)
+        this.setState({
+            id: 1
+        })
+        console.log(this.state.id)
+    }
+
     render(){
         return (
             <div className="main-edit-container">
-                <img src="https://lh3.googleusercontent.com/-Sh2ali7Rm1Q/AAAAAAAAAAI/AAAAAAAAAMY/eYgSvFha8ww/photo.jpg" className="user-profile-img" />
-                <div className="edit-info-container">
-                    <span className="username-title">Username</span>
-                    <input className="change-name"></input>
+            <div className="avatar-modal">
+                <div onClick={this.handleImgClick}>mountain</div>
+                {/* <div onClick={this.setState({id: 1})}>pizza</div>
+                <div onClick={this.setState({id: 2})}>cat</div>
+                <div onClick={this.setState({id: 3})}>plane</div> */}
+            </div>
+                <div className="edit-body">
+                    <div className="edit-pic-container">
+                        <img src="https://lh3.googleusercontent.com/-Sh2ali7Rm1Q/AAAAAAAAAAI/AAAAAAAAAMY/eYgSvFha8ww/photo.jpg" className="user-profile-img"></img>
+                        <button className="edit-buttons">Edit Portrait</button>
+                    </div>
+                    <div className="edit-info-container">
+                        <span className="username-title">Username</span>
+                        <input className="change-name"
+                            type="text" 
+                            onChange={e => this.setUsername(e.target.value)} 
+                            value={this.state.username}>
+                        </input>
+                        <span className="about-me-title">About Me</span>
+                        <textarea className="about-me"
+                            maxLength="300"type="text" 
+                            onChange={e => this.setAboutMe(e.target.value)} 
+                            value={this.state.aboutMe}>
+                        </textarea>
+                        <Link to="/profile">
+                            <button className="edit-buttons" style={{marginLeft: '400px', textDecoration: 'none'}}>Save Changes</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         )
