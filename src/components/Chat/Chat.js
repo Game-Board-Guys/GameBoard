a556504af90fdb07310ac90f693ef25b521ff0a7
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-02-22 15:13:06 -0700

    Chat component created

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Chat.css';
+import io from 'socket.io-client';
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userID: 1, // 1 is a placeholder until we have user accounts up.
+      messages: []
+    }
+
+    // ----sockets binds-----
+    this.updateMessages = this.updateMessages.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.setUserId = this.setUserId.bind(this);
+  }
+
+  componentDidMount() {
+    // this.state.sliderImages[0]
+    this.socket = io('/');
+    this.socket.on('message dispatched', this.updateMessages)
+    this.socket.on('welcome', this.setUserId)
+  }
+
+    // -------socket methods--------
+    updateMessages(message) {
+      const updatedMessages = this.state.messages.slice();
+      updatedMessages.push(message);
+      this.setState({
+        messages: updatedMessages
+      });
+      console.log(this.state.messages);
+    }
+
+    setUserId(user) {
+      this.setState(user);
+    }
+
+    sendMessage() {
+      const message = this.refs.message.value;
+      console.log(message);
+      this.socket.emit('message sent', { message, userid: this.state.user });
+      this.updateMessages({ message, user: this.state.userID });
+      this.refs.message.value = '';
+    }
+
+  render() {
+
+    const messages = this.state.messages.map((e, i) => {
+      const styles = e.user === this.state.userID ? { alignSelf: "flex-end", color: "#9C0D38", paddingLeft: "7px", fontWeight: "900", fontSize: "20px" } : { alignSelf: "flex-start", color: "#E9D758", fontWeight: "900", paddingLeft: "7px", fontSize: "20px" };
+      return (
+        <p key={i} style={styles}>{e.message}</p>
+      );
+    });
+
+    return (
+      <div className="chat">
+        <div className="landing-chat-box">
+          {messages}
+        </div>
+        <div className="chat-control">
+          <input className="chat-input" ref="message" />
+          <button className="chat-button" onClick={this.sendMessage}>Send</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chat;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
 import './Landing.css';
 import { Link } from 'react-router-dom';
 import checkers from './../../Pictures/checkers.jpeg';
 import blackjack from './../../Pictures/black-jack.jpg';
 import minesweeper from './../../Pictures/minesweeper.jpg';
+import Chat from '../Chat/Chat';
 import breakBrick from './../../Pictures/Screenshot (7).png';
 
 
@@ -15,23 +15,10 @@ class Landing extends Component {
         this.state = {
             // sliderImages: ["url('./../../Pictures/checkers.jpeg')","url('./../../Pictures/black-jack.jpg')", "url('./../../Pictures/minesweeper.jpg')", "url('./../../Pictures/Screenshot (7).png')"],
             sliderImages: [`url(${checkers}`, `url(${minesweeper}`, `url(${blackjack}`],
-            cur: 0,
-            userID: 1,
-            messages: []
+            cur: 0
         }
         this.slideLeft = this.slideLeft.bind(this);
         this.slideRight = this.slideRight.bind(this);
-        // ----sockets binds-----
-        this.updateMessages = this.updateMessages.bind(this);
-        this.sendMessage = this.sendMessage.bind(this);
-        this.setUserId = this.setUserId.bind(this);
-    }
-
-    componentDidMount() {
-        // this.state.sliderImages[0]
-        this.socket = io('/');
-        this.socket.on('message dispatched', this.updateMessages)
-        this.socket.on('welcome', this.setUserId)
     }
 
     slideLeft() {
@@ -61,40 +48,12 @@ class Landing extends Component {
         }
     }
 
-    // -------socket methods--------
-    updateMessages(message) {
-        const updatedMessages = this.state.messages.slice()
-        updatedMessages.push(message)
-        this.setState({
-            messages: updatedMessages
-        })
-        console.log(this.state.messages)
-    }
-
-    setUserId(user) {
-        this.setState(user)
-    }
-
-    sendMessage() {
-        const message = this.refs.message.value
-        console.log(message)
-        this.socket.emit('message sent', { message, userid: this.state.user })
-        this.updateMessages({ message, user: this.state.userID })
-        this.refs.message.value = '';
-    }
-
     render() {
         console.log('images here', this.state.sliderImages)
         const sliderStyle = {
             backgroundImage: this.state.sliderImages[this.state.cur]
         }
-        // -------sockets message display-------
-        const messages = this.state.messages.map((e, i) => {
-            const styles = e.user === this.state.userID ? { alignSelf: "flex-end", backgroundColor: "#2d96fb", color: "white" } : { alignSelf: "flex-start", backgroundColor: "#e5e6ea" }
-            return (
-                <p key={i} style={styles}>{e.message}</p>
-            )
-        })
+
         return (
             <div className="landing_main_container">
                 <div className="arrow-slider-container">
@@ -106,15 +65,7 @@ class Landing extends Component {
                     </div>
                     <div className="right-arrow" onClick={() => this.slideRight()}></div>
                 </div>
-                <div className="landing-chat-box">
-
-                    {/* <input type="text" placeholder="Enter a message..." onKeyUp={this.handleSubmit} />
-                    {this.state.messages} */}
-                    <input ref="message" />
-                    <button onClick={this.sendMessage}>Send</button>
-                    {messages}
-
-                </div>
+                <Chat />
                 <div className="game-list-container">
                     <Link to="lobby" style={{ textDecoration: 'none' }}>
                         <div className="game-container">
