340528de4c236395fac24dd01fea927cadcda62a
devin <deivnobowen@gmail.com>
2018-02-22 11:38:58 -0700

    sockets.io is working with a chat

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import './Landing.css';
 import { Link } from 'react-router-dom';
 import checkers from './../../Pictures/checkers.jpeg';
@@ -15,13 +16,22 @@ class Landing extends Component {
             // sliderImages: ["url('./../../Pictures/checkers.jpeg')","url('./../../Pictures/black-jack.jpg')", "url('./../../Pictures/minesweeper.jpg')", "url('./../../Pictures/Screenshot (7).png')"],
             sliderImages: [`url(${checkers}`, `url(${minesweeper}`, `url(${blackjack}`],
             cur: 0,
+            userID: 1,
+            messages: []
         }
         this.slideLeft = this.slideLeft.bind(this);
         this.slideRight = this.slideRight.bind(this);
+        // ----sockets binds-----
+        this.updateMessages = this.updateMessages.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.setUserId = this.setUserId.bind(this);
     }
 
     componentDidMount() {
         // this.state.sliderImages[0]
+        this.socket = io('/');
+        this.socket.on('message dispatched', this.updateMessages)
+        this.socket.on('welcome', this.setUserId)
     }
 
     slideLeft() {
@@ -51,11 +61,40 @@ class Landing extends Component {
         }
     }
 
-    render(){
+    // -------socket methods--------
+    updateMessages(message) {
+        const updatedMessages = this.state.messages.slice()
+        updatedMessages.push(message)
+        this.setState({
+            messages: updatedMessages
+        })
+        console.log(this.state.messages)
+    }
+
+    setUserId(user) {
+        this.setState(user)
+    }
+
+    sendMessage() {
+        const message = this.refs.message.value
+        console.log(message)
+        this.socket.emit('message sent', { message, userid: this.state.user })
+        this.updateMessages({ message, user: this.state.userID })
+        this.refs.message.value = '';
+    }
+
+    render() {
         console.log('images here', this.state.sliderImages)
-        const sliderStyle={
+        const sliderStyle = {
             backgroundImage: this.state.sliderImages[this.state.cur]
         }
+        // -------sockets message display-------
+        const messages = this.state.messages.map((e, i) => {
+            const styles = e.user === this.state.userID ? { alignSelf: "flex-end", backgroundColor: "#2d96fb", color: "white" } : { alignSelf: "flex-start", backgroundColor: "#e5e6ea" }
+            return (
+                <p key={i} style={styles}>{e.message}</p>
+            )
+        })
         return (
             <div className="landing_main_container">
                 <div className="arrow-slider-container">
@@ -68,31 +107,34 @@ class Landing extends Component {
                     <div className="right-arrow" onClick={() => this.slideRight()}></div>
                 </div>
                 <div className="landing-chat-box">
-                
-                <input type="text" placeholder="Enter a message..." onKeyUp={this.handleSubmit} />
-                {this.state.messages}
-                
+
+                    {/* <input type="text" placeholder="Enter a message..." onKeyUp={this.handleSubmit} />
+                    {this.state.messages} */}
+                    <input ref="message" />
+                    <button onClick={this.sendMessage}>Send</button>
+                    {messages}
+
                 </div>
                 <div className="game-list-container">
-                    <Link to="lobby" style={{textDecoration: 'none'}}>
+                    <Link to="lobby" style={{ textDecoration: 'none' }}>
                         <div className="game-container">
                             <div className="listed-game-1"></div>
                             <div className="game-text">Checkers</div>
                         </div>
                     </Link>
-                    <Link to="lobby" style={{textDecoration: 'none'}}>
+                    <Link to="lobby" style={{ textDecoration: 'none' }}>
                         <div className="game-container">
                             <div className="listed-game-2"></div>
                             <div className="game-text">Black Jack</div>
                         </div>
                     </Link>
-                    <Link to="lobby" style={{textDecoration: 'none'}}>
+                    <Link to="lobby" style={{ textDecoration: 'none' }}>
                         <div className="game-container">
                             <div className="listed-game-3"></div>
                             <div className="game-text">Mine Sweeper</div>
                         </div>
                     </Link>
-                    <Link to="lobby" style={{textDecoration: 'none'}}>
+                    <Link to="lobby" style={{ textDecoration: 'none' }}>
                         <div className="game-container">
                             <div className="listed-game-4"></div>
                             <div className="game-text">Break Brick</div>
