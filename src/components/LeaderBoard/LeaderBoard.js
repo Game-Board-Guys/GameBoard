a9f90b079825009c5da654cd342b23b3acd3ee19
blairLikesPizza <blairwo@gmail.com>
2018-02-22 14:02:59 -0700

    leaderboard styling

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -7,10 +7,94 @@ class LeaderBoard extends Component {
         return (
             <div className="main-leaderboard-container">
                 <div className="leaderboard-sidebar">
-
+                    <div className="sidebar-header">Select Game</div>
+                    <div className="game-name-box">
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        <p>Pizza</p>
+                        
+                    </div>
                 </div>
-                <div>
-                    
+                <div className="game-leaderboard-container">
+                    <h1 className="leaderboard-game-header">Leaderboard</h1>
+                    <div className="game-leaderboard-box"></div>
                 </div>
             </div>
         )
--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Lobby.css';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 class Lobby extends Component {
     render(){
@@ -9,10 +10,10 @@ class Lobby extends Component {
                 <canvas id="myCanvas"></canvas>
                 <div className="upper-lobby-right">
                 <div className="chat-bar"></div>
-                 <div className="lobby-buttons"><button>Rules</button>   <button>Leave Game</button></div>
+                 <div className="lobby-buttons"><a href="#RulesContainer">Rules</a>   <button>Leave Game</button></div>
                 </div>
                 </div>
-                <div className="rules-container"></div>
+                <ScrollableAnchor className="rules-container" id={"RulesContainer"}></ScrollableAnchor>
                 <div className="leave-game-button"></div>
             </div>
         )
