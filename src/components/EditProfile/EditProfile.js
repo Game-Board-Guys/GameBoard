1ce2b8f56a827f3cc3067daf63d90311077d1f87
blairLikesPizza <blairwo@gmail.com>
2018-02-28 16:06:20 -0700

    added more avatars

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -25,6 +25,19 @@ class EditProfile extends Component {
         this.handleImg6Click = this.handleImg6Click.bind(this);
         this.handleImg7Click = this.handleImg7Click.bind(this);
         this.handleImg8Click = this.handleImg8Click.bind(this);
+        this.handleImg9Click = this.handleImg9Click.bind(this);
+        this.handleImg10Click = this.handleImg10Click.bind(this);
+        this.handleImg11Click = this.handleImg11Click.bind(this);
+        this.handleImg12Click = this.handleImg12Click.bind(this);
+        this.handleImg13Click = this.handleImg13Click.bind(this);
+        this.handleImg14Click = this.handleImg14Click.bind(this);
+        this.handleImg15Click = this.handleImg15Click.bind(this);
+        this.handleImg16Click = this.handleImg16Click.bind(this);
+        this.handleImg17Click = this.handleImg17Click.bind(this);
+        this.handleImg18Click = this.handleImg18Click.bind(this);
+        this.handleImg19Click = this.handleImg19Click.bind(this);
+        this.handleImg20Click = this.handleImg20Click.bind(this);
+        this.handleImg21Click = this.handleImg21Click.bind(this);
     }
 
     componentDidMount(){
@@ -112,6 +125,97 @@ class EditProfile extends Component {
             show: false
         })
     }
+    
+    handleImg9Click() {
+        this.setState({
+            img: 'https://i.imgur.com/xOwTCBh.jpg',
+            show: false
+        })
+    }
+    
+    handleImg10Click() {
+        this.setState({
+            img: 'https://i.imgur.com/tz4E54E.jpg',
+            show: false
+        })
+    }
+    
+    handleImg11Click() {
+        this.setState({
+            img: 'https://i.imgur.com/1DfL734.jpg',
+            show: false
+        })
+    }
+    
+    handleImg12Click() {
+        this.setState({
+            img: 'https://i.imgur.com/xqvHKng.jpg',
+            show: false
+        })
+    }
+    
+    handleImg13Click() {
+        this.setState({
+            img: 'https://i.imgur.com/diTObOl.jpg',
+            show: false
+        })
+    }
+    
+    handleImg14Click() {
+        this.setState({
+            img: 'https://i.imgur.com/KZbROKV.jpg',
+            show: false
+        })
+    }
+    
+    handleImg15Click() {
+        this.setState({
+            img: 'https://i.imgur.com/tGgRip2.jpg',
+            show: false
+        })
+    }
+    
+    handleImg16Click() {
+        this.setState({
+            img: 'https://i.imgur.com/gSWD3ia.jpg',
+            show: false
+        })
+    }
+    
+    handleImg17Click() {
+        this.setState({
+            img: 'https://i.imgur.com/1sN8zNH.jpg',
+            show: false
+        })
+    }
+    
+    handleImg18Click() {
+        this.setState({
+            img: 'https://i.imgur.com/VeB79d4.jpg',
+            show: false
+        })
+    }
+    
+    handleImg19Click() {
+        this.setState({
+            img: 'https://i.imgur.com/UUDP2uj.png',
+            show: false
+        })
+    }
+    
+    handleImg20Click() {
+        this.setState({
+            img: 'https://i.imgur.com/3AFY66F.jpg',
+            show: false
+        })
+    }
+    
+    handleImg21Click() {
+        this.setState({
+            img: 'https://i.imgur.com/eFeqD34.jpg',
+            show: false
+        })
+    }
 
     showModal = () => {
         this.setState({
@@ -139,6 +243,19 @@ class EditProfile extends Component {
                 showChosenImg6={this.handleImg6Click}
                 showChosenImg7={this.handleImg7Click}
                 showChosenImg8={this.handleImg8Click}
+                showChosenImg9={this.handleImg9Click}
+                showChosenImg10={this.handleImg10Click}
+                showChosenImg11={this.handleImg11Click}
+                showChosenImg12={this.handleImg12Click}
+                showChosenImg13={this.handleImg13Click}
+                showChosenImg14={this.handleImg14Click}
+                showChosenImg15={this.handleImg15Click}
+                showChosenImg16={this.handleImg16Click}
+                showChosenImg17={this.handleImg17Click}
+                showChosenImg18={this.handleImg18Click}
+                showChosenImg19={this.handleImg19Click}
+                showChosenImg20={this.handleImg20Click}
+                showChosenImg21={this.handleImg21Click}
                 onClose1={this.showModal}
                 onClose2={this.showModal}
                 onClose3={this.showModal}
@@ -147,6 +264,19 @@ class EditProfile extends Component {
                 onClose6={this.showModal}
                 onClose7={this.showModal}
                 onClose8={this.showModal}
+                onClose9={this.showModal}
+                onClose10={this.showModal}
+                onClose11={this.showModal}
+                onClose12={this.showModal}
+                onClose13={this.showModal}
+                onClose14={this.showModal}
+                onClose15={this.showModal}
+                onClose16={this.showModal}
+                onClose17={this.showModal}
+                onClose18={this.showModal}
+                onClose19={this.showModal}
+                onClose20={this.showModal}
+                onClose21={this.showModal}
                 // closeModal={this.showModal}
                 show={this.state.show}>
             </Modal>
